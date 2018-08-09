dc6ab889f0beff1967dea71c0e20d5c469deed0e
PBo <sl9site@gmail.com>
2018-08-08 21:50:47 -0700

    reBuild

--- app.js
+++ app.js
@@ -1,133 +1,69 @@
-let a = null;
-let b = null;
-let operation = "";
-let skipPrevRead = false;
+let firstValue = "";
+let secondValue = "";
+let operandValue = "";
 
-function read() {
-	return document.getElementById("result").value || "0";
-}
+//let skipPrevRead = false;
 
-function write(value) {
-	document.getElementById("result").value = value;
-}
-
-function addToList(nText) {
-	let element = document.getElementById("calcStatusText");
-	element.innerHTML += nText;
-}
-
-function toInput(value) {
-	if (skipPrevRead) {
-		write("");
-		skipPrevRead = false;
-	}
-	if (value === "." && !read().includes(".")) {
-		write(read() + value);
-	} else if (value === "0" && read().includes(".")) {
-		write(read() + value);
-	} else if (value !== "0" && value !== ".") {
-		if (read().includes(".")) {
-			write(read() + value);
-		} else {
-			if (read()[0] === "0") {
-				write(read().substring(1) + value);
-			} else {
-				write(read() + value);
-			}
-		}
-	}
-	addToList(value);
-}
-
-function subtract() {
-	addToList("-");
-	if (!operation) {
-		a = Number(read());
-		operation = "-";
-		skipPrevRead = true;
+function toInput(n) {
+	if (operandValue) {
+		secondValue += n;
+		write(secondValue);
 	} else {
-		getResult();
+		firstValue += n;
+		write(firstValue);
 	}
 }
 
-function addNum() {
-	addToList("+");
-	if (!operation) {
-		a = Number(read());
-		operation = "+";
-		skipPrevRead = true;
-	} else {
-		getResult();
-	}
+function write(n) {
+	document.getElementById("display").value = n;
 }
 
-function multiply() {
-	addToList("*");
-	if (!operation) {
-		a = Number(read());
-		operation = "*";
-		skipPrevRead = true;
-	} else {
-		getResult();
-	}
-}
-
-function divide() {
-	addToList("/");
-	if (!operation) {
-		a = Number(read());
-		operation = "/";
-		skipPrevRead = true;
-	} else {
-		getResult();
-	}
+function operand(operand) {
+	write("");
+	operandValue = operand;
 }
 
 function getResult() {
-	addToList("=");
-	b = Number(read());
-	if (operation) {
-		switch (operation) {
-		case "-":
-			write(a - b);
-			a = Number(read());
-			b = null;
-			break;
-		case "+":
-			write(a + b);
-			a = Number(read());
-			b = null;
-			break;
-		case "*":
-			write(a * b);
-			a = Number(read());
-			b = null;
-			break;
-		case "/":
-			write(a / b);
-			a = Number(read());
-			b = null;
-			break;
-		}
+	if (operandValue === "+") {
+		write(+firstValue + +secondValue);
 	}
-	addToList(read() + "; ");
-	operation = "";
-	skipPrevRead = true;
+	if (operandValue === "-") {
+		write(+firstValue - +secondValue);
+	}
+	if (operandValue === "*") {
+		write(+firstValue * +secondValue);
+	}
+	if (operandValue === "/") {
+		write(+firstValue / +secondValue);
+	}
 }
+
 
 function clearValue() {
-	// console.log('clear value');
-	write("0");
-	a = b = null;
+  write("0");
+	firstValue = secondValue = '';
 	operation = "";
-	let element = document.getElementById("calcStatusText");
-	element.innerHTML = "";
 }
 
-function sum(a, b) {
-	return a + b;
-}
+
+
+//
+// function addToList(Text) {
+// 	let element = document.getElementById("historyLog");
+// 	element.innerHTML += Text;
+// }
+
+
+// function clearValue() {
+// 	// console.log('clear value');
+// 	write("0");
+// 	firstValue = secondValue = null;
+// 	operation = "";
+// 	let element = document.getElementById("historyLog");
+// 	element.innerHTML = "";
+// }
+
 
 module.exports = {
-	sum
+
 };
