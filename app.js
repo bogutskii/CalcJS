4673d175bdf9372c7b3013110c3cb4accbefc913
PBo <sl9site@gmail.com>
2018-08-09 21:50:45 -0700

    fixed 0.1

--- app.js
+++ app.js
@@ -1,69 +1,74 @@
-let firstValue = "";
-let secondValue = "";
-let operandValue = "";
+let firstValue = '';
+let secondValue = '';
+let operandValue = '';
 
-//let skipPrevRead = false;
+// let skipPrevRead = false;
 
 function toInput(n) {
-	if (operandValue) {
-		secondValue += n;
-		write(secondValue);
-	} else {
-		firstValue += n;
-		write(firstValue);
-	}
+  if (operandValue) {
+    secondValue += n;
+    write(secondValue);
+  } else {
+    firstValue += n;
+    write(firstValue);
+  }
 }
 
 function write(n) {
-	document.getElementById("display").value = n;
+  document.getElementById('display').value = n;
 }
 
 function operand(operand) {
-	write("");
-	operandValue = operand;
+  write('');
+  operandValue = operand;
 }
+
+function clearValue(firstValueByDefault = '') {
+  firstValue = firstValueByDefault;
+  secondValue = '';
+  operandValue = '';
+}
+
 
 function getResult() {
-	if (operandValue === "+") {
-		write(+firstValue + +secondValue);
-	}
-	if (operandValue === "-") {
-		write(+firstValue - +secondValue);
-	}
-	if (operandValue === "*") {
-		write(+firstValue * +secondValue);
-	}
-	if (operandValue === "/") {
-		write(+firstValue / +secondValue);
-	}
+  let result = 0;
+
+  if (operandValue === '+') {
+    result = +firstValue + +secondValue;
+  }
+  if (operandValue === '-') {
+    result = +firstValue - +secondValue;
+  }
+  if (operandValue === '*') {
+    result = +firstValue * +secondValue;
+  }
+  if (operandValue === '/') {
+    result = +firstValue / +secondValue;
+  }
+  write(result);
+  clearValue(result);
 }
-
-
-function clearValue() {
-  write("0");
-	firstValue = secondValue = '';
-	operation = "";
-}
-
-
-
+//
+// function clearValue() {
+//   write("0");
+//   firstValue = secondValue = '';
+//   operation = "";
+// }
+//   result = +firstValue * +secondValue;
+// }
+// if (operandValue === '/') {
+// if (operandValue === '*') {
+// }
+//   result = +firstValue / +secondValue;
 //
 // function addToList(Text) {
 // 	let element = document.getElementById("historyLog");
 // 	element.innerHTML += Text;
 // }
 
-
-// function clearValue() {
-// 	// console.log('clear value');
-// 	write("0");
-// 	firstValue = secondValue = null;
-// 	operation = "";
 // 	let element = document.getElementById("historyLog");
 // 	element.innerHTML = "";
 // }
 
 
-module.exports = {
-
-};
+module.exports = {};
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-const { sum } = require("./app.js");
+const { sum } = require('./app.js');
 
 function sumMY(a, b) {
-	return a + b;
+  return a + b;
 }
 
 
-test("Should sum works", () => {
-	expect(sum(2, 3)).toBe(5);
+test('Should sum works', () => {
+  expect(sum(2, 3)).toBe(5);
 });
